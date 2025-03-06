28b753c6ac2463cc8bec26207545d6a80dc3a45c
unknown <Rabi Gorkhali>
2025-03-06 10:02:08 +0545

    fix: config use real name

--- app/Services/ConfigService.php
+++ app/Services/ConfigService.php
@@ -25,30 +25,18 @@ class ConfigService extends Service
     {
         $data = $request->except('_token');
         if ($request->hasFile('logo')) {
-            $file = $request->file('logo');
-            $filename = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path(getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model)))), $filename);
-            $data['logo'] = getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model))).'/'.$filename;
+            $data['logo'] = $this->fullImageUploadPath . uploadImage($this->fullImageUploadPath, 'logo', false, null, null);
         }
 
         if ($request->hasFile('secondary_logo')) {
-            $file = $request->file('secondary_logo');
-            $filename = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path(getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model)))), $filename);
-            $data['secondary_logo'] = getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model))).'/'.$filename;
+            $data['secondary_logo'] = $this->fullImageUploadPath . uploadImage($this->fullImageUploadPath, 'secondary_logo', false, null, null);
         }
 
         if ($request->hasFile('favicon')) {
-            $file = $request->file('favicon');
-            $filename = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path(getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model)))), $filename);
-            $data['favicon'] = getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model))).'/'.$filename;
+            $data['favicon'] = $this->fullImageUploadPath . uploadImage($this->fullImageUploadPath, 'favicon', false, null, null);
         }
         if ($request->hasFile('bank_qr')) {
-            $file = $request->file('bank_qr');
-            $filename = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path(getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model)))), $filename);
-            $data['bank_qr'] = getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model))).'/'.$filename;
+            $data['bank_qr'] = $this->fullImageUploadPath . uploadImage($this->fullImageUploadPath, 'bank_qr', false, null, null);
         }
 
         return $this->model->create($data);
@@ -60,45 +48,33 @@ class ConfigService extends Service
         $update = $this->itemByIdentifier($id);
         $faviconPath = $update->favicon ?? null;
         $logoPath = $update->logo ?? null;
+        $secondaryLogoPath = $update->secondary_logo ?? null;
         $bankQrPath = $update->bank_qr ?? null;
-        $secondaryPath = $update->secondary_logo ?? null;
         if ($request->hasFile('favicon')) {
             if ($faviconPath && file_exists(public_path($faviconPath))) {
                 unlink(public_path($faviconPath));
             }
-            $file = $request->file('favicon');
-            $filename = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path(getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model)))), $filename);
-            $data['favicon'] = getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model))).'/'.$filename;
-        }
+            $data['favicon'] = $this->fullImageUploadPath . uploadImage($this->fullImageUploadPath, 'favicon', false, null, null);
 
+        }
         if ($request->hasFile('secondary_logo')) {
-            if ($secondaryPath && file_exists(public_path($secondaryPath))) {
-                unlink(public_path($secondaryPath));
+            if ($secondaryLogoPath && file_exists(public_path($secondaryLogoPath))) {
+                unlink(public_path($secondaryLogoPath));
             }
-            $file = $request->file('secondary_logo');
-            $filename = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path(getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model)))), $filename);
-            $data['secondary_logo'] = getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model))).'/'.$filename;
-        }
+            $data['secondary_logo'] = $this->fullImageUploadPath . uploadImage($this->fullImageUploadPath, 'secondary_logo', false, null, null);
 
+        }
         if ($request->hasFile('bank_qr')) {
             if ($bankQrPath && file_exists(public_path($bankQrPath))) {
                 unlink(public_path($bankQrPath));
             }
-            $file = $request->file('bank_qr');
-            $filename = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path(getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model)))), $filename);
-            $data['bank_qr'] = getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model))).'/'.$filename;
+            $data['bank_qr'] = $this->fullImageUploadPath . uploadImage($this->fullImageUploadPath, 'bank_qr', false, null, null);
         }
         if ($request->hasFile('logo')) {
             if ($logoPath && file_exists(public_path($logoPath))) {
                 unlink(public_path($logoPath));
             }
-            $file = $request->file('logo');
-            $filename = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path(getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model)))), $filename);
-            $data['logo'] = getImageUploadFirstLevelPath().'/'.strtolower(class_basename(get_class($this->model))).'/'.$filename;
+            $data['logo'] = $this->fullImageUploadPath . uploadImage($this->fullImageUploadPath, 'logo', false, null, null);
         }
         $update->fill($data)->save();
         $update = $this->itemByIdentifier($id);
